9aaf365acfaab05ba334c519aae64f3774a68320
Srilatha-1232 <mullamurisrilatha43@gmail.com>
2026-02-09 21:37:51 +0530

    Add files via upload

---
+++ JS-1-1.js
@@ -0,0 +1,18 @@
+//  javascript basics lesson 1.1 - HW
+
+// 1.a alert to display good morning 
+alert('Good Morning!');
+
+// 1.b Display Your Name in popup
+alert('Rama Krishna!');
+
+// 1.c use math calculate 10 + 5 in console 
+//  10 + 5 => 15
+
+// 1.d  calculate 20 -5 in the console
+//  20 - 5 => 15
+
+//  1.e calculate 2+2-5 in the console
+// 2+2 -5 => -1
+
+// exercise 1.1 png is completed these are the answers
